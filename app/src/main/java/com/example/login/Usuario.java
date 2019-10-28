eee2b263badc634d20e0ed12cb2e12099863e3a5
UMovil <50064656+UMovil@users.noreply.github.com>
2019-10-28 18:28:04 -0500

    Version 1.0 --> Se juntó los dos codigos.

--- app/src/main/java/com/example/login/Usuario.java
+++ app/src/main/java/com/example/login/Usuario.java
@@ -1,60 +1,115 @@
 package com.example.login;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
-public class Usuario extends AppCompatActivity implements View.OnClickListener {
+public class Usuario extends AppCompatActivity {
 
-    TextView N, C, P;
-    Button bs;
-    int Id;
-    daoUsuario dao;
-    usuarios u;
+    //PREGUNTA 1
+    private RadioButton rb_p1a, rb_p1b, rb_p1c, rb_p1d;
+    private RadioGroup rbg1;
+    public double nota_rp1 = 1.00;
 
+    //PREGUNTA 2
+    private RadioButton rb_p2a, rb_p2b, rb_p2c, rb_p2d;
+    private RadioGroup rbg2;
+    public double nota_rp2 = 1.00;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_usuario);
 
-        N = (TextView)findViewById(R.id.nom_us);
-        /*
-        C = (TextView)findViewById(R.id.C);
-        P = (TextView)findViewById(R.id.pass);*/
-        bs = (Button)findViewById(R.id.bs);
+        //PREGUNTA 1
+        rb_p1a = (RadioButton)findViewById(R.id.p1a);
+        rb_p1b = (RadioButton)findViewById(R.id.p1b);
+        rb_p1c = (RadioButton)findViewById(R.id.p1c);
+        rb_p1d = (RadioButton)findViewById(R.id.p1d);
+        rbg1 = (RadioGroup)findViewById(R.id.p1);
 
-        bs.setOnClickListener(this);
-
-
-
-         Bundle b =getIntent().getExtras();
-        Id =b.getInt("Id");
-        dao = new daoUsuario(this);
-        u=dao.getusuariosById(Id);
-
-       // N.setText(u.getCorreo());
+        //PREGUNTA 2
+        rb_p2a = (RadioButton)findViewById(R.id.p2a);
+        rb_p2b = (RadioButton)findViewById(R.id.p2b);
+        rb_p2c = (RadioButton)findViewById(R.id.p2c);
+        rb_p2d = (RadioButton)findViewById(R.id.p2d);
+        rbg2 = (RadioGroup)findViewById(R.id.p2);
 
     }
+    public void v_p1(View view){
 
-    @Override
-    public void onClick(View v) {
+        Button boton = (Button) findViewById(R.id.bt_vp1);
 
-        switch (v.getId()){
+        if (rb_p1a.isChecked()){
 
-            case R.id.bs:
-                Intent i = new Intent(Usuario.this, MainActivity.class);
-                startActivity(i);
-                break;
+            rb_p1a.setEnabled(false);rb_p1a.setClickable(false);rb_p1a.setTextColor(Color.parseColor("#ff3333"));
+            nota_rp1 = nota_rp1 - 0.25;
+            Toast.makeText(getApplicationContext(), "La respuesta escogida no es la correcta, intenta escogiendo otra respuesta.", Toast.LENGTH_LONG).show();
+            rbg1.clearCheck();
 
+        }else if (rb_p1b.isChecked()){
 
+            rb_p1b.setEnabled(false);rb_p1b.setClickable(false);rb_p1b.setTextColor(Color.parseColor("#ff3333"));
+            nota_rp1 = nota_rp1 - 0.25;
+            Toast.makeText(getApplicationContext(), "La respuesta escogida no es la correcta, intenta escogiendo otra respuesta.", Toast.LENGTH_LONG).show();
+            rbg1.clearCheck();
 
+        }else if (rb_p1c.isChecked()){
 
+            rb_p1c.setEnabled(false);rb_p1c.setClickable(false);rb_p1c.setTextColor(Color.parseColor("#ff3333"));
+            nota_rp1 = nota_rp1 - 0.25;
+            Toast.makeText(getApplicationContext(), "La respuesta escogida no es la correcta, intenta escogiendo otra respuesta.", Toast.LENGTH_LONG).show();
+            rbg1.clearCheck();
+
+        }else if (rb_p1d.isChecked()) {
+
+            rb_p1a.setEnabled(false); rb_p1a.setClickable(false);
+            rb_p1b.setEnabled(false); rb_p1b.setClickable(false);
+            rb_p1c.setEnabled(false); rb_p1c.setClickable(false);
+            rb_p1d.setTextColor(Color.parseColor("#33FF49"));
+            boton.setEnabled(false);
+            Toast.makeText(getApplicationContext(), "Excelente , haz acertado la respuesta correcta. ", Toast.LENGTH_SHORT).show();
         }
+    }
+    public void v_p2(View view){
 
+        Button boton = (Button) findViewById(R.id.bt_vp2);
+
+        if (rb_p2a.isChecked()){
+
+            rb_p2a.setEnabled(false);rb_p2a.setClickable(false);rb_p2a.setTextColor(Color.parseColor("#ff3333"));
+            nota_rp2 = nota_rp2 - 0.25;
+            Toast.makeText(getApplicationContext(), "La respuesta escogida no es la correcta, intenta escogiendo otra respuesta.", Toast.LENGTH_LONG).show();
+            rbg2.clearCheck();
+
+        }else if (rb_p2b.isChecked()){
+
+            rb_p2b.setEnabled(false);rb_p2b.setClickable(false);rb_p2b.setTextColor(Color.parseColor("#ff3333"));
+            nota_rp2 = nota_rp2 - 0.25;
+            Toast.makeText(getApplicationContext(), "La respuesta escogida no es la correcta, intenta escogiendo otra respuesta.", Toast.LENGTH_LONG).show();
+            rbg2.clearCheck();
+
+        }else if (rb_p2c.isChecked()){
+
+            rb_p2c.setEnabled(false);rb_p2c.setClickable(false);rb_p2c.setTextColor(Color.parseColor("#ff3333"));
+            nota_rp2 = nota_rp2 - 0.25;
+            Toast.makeText(getApplicationContext(), "La respuesta escogida no es la correcta, intenta escogiendo otra respuesta.", Toast.LENGTH_LONG).show();
+            rbg2.clearCheck();
+
+        }else if (rb_p2d.isChecked()) {
+
+            rb_p2a.setEnabled(false); rb_p2a.setClickable(false);
+            rb_p2b.setEnabled(false); rb_p2b.setClickable(false);
+            rb_p2c.setEnabled(false); rb_p2c.setClickable(false);
+            rb_p2d.setTextColor(Color.parseColor("#33FF49"));
+            boton.setEnabled(false);
+            Toast.makeText(getApplicationContext(), "Excelente , haz acertado la respuesta correcta. ", Toast.LENGTH_SHORT).show();
+        }
     }
 }
+
