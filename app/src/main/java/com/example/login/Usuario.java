8bb00362031c08d0a0744b163ba58333ee994f52
UMovil <50064656+UMovil@users.noreply.github.com>
2019-10-28 22:43:23 -0500

    Version 1.1 --> Se corrigieron errores y se obtuvo la nota final con condicionales if.

--- app/src/main/java/com/example/login/Usuario.java
+++ app/src/main/java/com/example/login/Usuario.java
@@ -11,6 +11,7 @@ import android.widget.Toast;
 
 public class Usuario extends AppCompatActivity {
 
+
     //PREGUNTA 1
     private RadioButton rb_p1a, rb_p1b, rb_p1c, rb_p1d;
     private RadioGroup rbg1;
@@ -20,6 +21,11 @@ public class Usuario extends AppCompatActivity {
     private RadioButton rb_p2a, rb_p2b, rb_p2c, rb_p2d;
     private RadioGroup rbg2;
     public double nota_rp2 = 1.00;
+
+    //NOTA ALMACENADA
+    double nota_final;
+    String stringdouble = Double.toString(nota_final);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,21 +55,24 @@ public class Usuario extends AppCompatActivity {
 
             rb_p1a.setEnabled(false);rb_p1a.setClickable(false);rb_p1a.setTextColor(Color.parseColor("#ff3333"));
             nota_rp1 = nota_rp1 - 0.25;
-            Toast.makeText(getApplicationContext(), "La respuesta escogida no es la correcta, intenta escogiendo otra respuesta.", Toast.LENGTH_LONG).show();
+//            Toast.makeText(getApplicationContext(), "La respuesta escogida no es la correcta, intenta escogiendo otra respuesta.", Toast.LENGTH_LONG).show();
+            Toast.makeText(getApplicationContext(), "La nota guardada es " + nota_rp1, Toast.LENGTH_SHORT).show();
             rbg1.clearCheck();
 
         }else if (rb_p1b.isChecked()){
 
             rb_p1b.setEnabled(false);rb_p1b.setClickable(false);rb_p1b.setTextColor(Color.parseColor("#ff3333"));
             nota_rp1 = nota_rp1 - 0.25;
-            Toast.makeText(getApplicationContext(), "La respuesta escogida no es la correcta, intenta escogiendo otra respuesta.", Toast.LENGTH_LONG).show();
+//            Toast.makeText(getApplicationContext(), "La respuesta escogida no es la correcta, intenta escogiendo otra respuesta.", Toast.LENGTH_LONG).show();
+            Toast.makeText(getApplicationContext(), "La nota guardada es " + nota_rp1, Toast.LENGTH_SHORT).show();
             rbg1.clearCheck();
 
         }else if (rb_p1c.isChecked()){
 
             rb_p1c.setEnabled(false);rb_p1c.setClickable(false);rb_p1c.setTextColor(Color.parseColor("#ff3333"));
             nota_rp1 = nota_rp1 - 0.25;
-            Toast.makeText(getApplicationContext(), "La respuesta escogida no es la correcta, intenta escogiendo otra respuesta.", Toast.LENGTH_LONG).show();
+//            Toast.makeText(getApplicationContext(), "La respuesta escogida no es la correcta, intenta escogiendo otra respuesta.", Toast.LENGTH_LONG).show();
+            Toast.makeText(getApplicationContext(), "La nota guardada es " + nota_rp1, Toast.LENGTH_SHORT).show();
             rbg1.clearCheck();
 
         }else if (rb_p1d.isChecked()) {
@@ -71,9 +80,11 @@ public class Usuario extends AppCompatActivity {
             rb_p1a.setEnabled(false); rb_p1a.setClickable(false);
             rb_p1b.setEnabled(false); rb_p1b.setClickable(false);
             rb_p1c.setEnabled(false); rb_p1c.setClickable(false);
+            rb_p1d.setClickable(false);
             rb_p1d.setTextColor(Color.parseColor("#33FF49"));
             boton.setEnabled(false);
-            Toast.makeText(getApplicationContext(), "Excelente , haz acertado la respuesta correcta. ", Toast.LENGTH_SHORT).show();
+//            Toast.makeText(getApplicationContext(), "Excelente , haz acertado la respuesta correcta. ", Toast.LENGTH_SHORT).show();
+            Toast.makeText(getApplicationContext(), "La nota guardada es " + nota_rp1, Toast.LENGTH_SHORT).show();
         }
     }
     public void v_p2(View view){
@@ -84,21 +95,24 @@ public class Usuario extends AppCompatActivity {
 
             rb_p2a.setEnabled(false);rb_p2a.setClickable(false);rb_p2a.setTextColor(Color.parseColor("#ff3333"));
             nota_rp2 = nota_rp2 - 0.25;
-            Toast.makeText(getApplicationContext(), "La respuesta escogida no es la correcta, intenta escogiendo otra respuesta.", Toast.LENGTH_LONG).show();
+//            Toast.makeText(getApplicationContext(), "La respuesta escogida no es la correcta, intenta escogiendo otra respuesta.", Toast.LENGTH_LONG).show();
+            Toast.makeText(getApplicationContext(), "La nota guardada es " + nota_rp2, Toast.LENGTH_SHORT).show();
             rbg2.clearCheck();
 
         }else if (rb_p2b.isChecked()){
 
             rb_p2b.setEnabled(false);rb_p2b.setClickable(false);rb_p2b.setTextColor(Color.parseColor("#ff3333"));
             nota_rp2 = nota_rp2 - 0.25;
-            Toast.makeText(getApplicationContext(), "La respuesta escogida no es la correcta, intenta escogiendo otra respuesta.", Toast.LENGTH_LONG).show();
+//            Toast.makeText(getApplicationContext(), "La respuesta escogida no es la correcta, intenta escogiendo otra respuesta.", Toast.LENGTH_LONG).show();
+            Toast.makeText(getApplicationContext(), "La nota guardada es " + nota_rp2, Toast.LENGTH_SHORT).show();
             rbg2.clearCheck();
 
         }else if (rb_p2c.isChecked()){
 
             rb_p2c.setEnabled(false);rb_p2c.setClickable(false);rb_p2c.setTextColor(Color.parseColor("#ff3333"));
             nota_rp2 = nota_rp2 - 0.25;
-            Toast.makeText(getApplicationContext(), "La respuesta escogida no es la correcta, intenta escogiendo otra respuesta.", Toast.LENGTH_LONG).show();
+//            Toast.makeText(getApplicationContext(), "La respuesta escogida no es la correcta, intenta escogiendo otra respuesta.", Toast.LENGTH_LONG).show();
+            Toast.makeText(getApplicationContext(), "La nota guardada es " + nota_rp2, Toast.LENGTH_SHORT).show();
             rbg2.clearCheck();
 
         }else if (rb_p2d.isChecked()) {
@@ -106,10 +120,28 @@ public class Usuario extends AppCompatActivity {
             rb_p2a.setEnabled(false); rb_p2a.setClickable(false);
             rb_p2b.setEnabled(false); rb_p2b.setClickable(false);
             rb_p2c.setEnabled(false); rb_p2c.setClickable(false);
+            rb_p2d.setClickable(false);
             rb_p2d.setTextColor(Color.parseColor("#33FF49"));
             boton.setEnabled(false);
-            Toast.makeText(getApplicationContext(), "Excelente , haz acertado la respuesta correcta. ", Toast.LENGTH_SHORT).show();
+//            Toast.makeText(getApplicationContext(), "Excelente , haz acertado la respuesta correcta. ", Toast.LENGTH_SHORT).show();
+            Toast.makeText(getApplicationContext(), "La nota guardada es " + nota_rp2, Toast.LENGTH_SHORT).show();
         }
     }
+    public void btotal (View view){
+
+        Button boton = (Button) findViewById(R.id.total);
+        Button boton1 = (Button) findViewById(R.id.bt_vp1);
+        Button boton2 = (Button) findViewById(R.id.bt_vp2);
+
+        if (boton1.isEnabled()) {
+            Toast.makeText(getApplicationContext(), "Aún no has terminado la pregunta 1", Toast.LENGTH_SHORT).show();
+        }else if (boton2.isEnabled()){
+            Toast.makeText(getApplicationContext(), "Aún no has terminado la pregunta 2", Toast.LENGTH_SHORT).show();
+        }else {
+            nota_final = nota_rp2 + nota_rp1;
+            Toast.makeText(getApplicationContext(), "La nota guardada es " + nota_final, Toast.LENGTH_SHORT).show();
+        }
+
+      }
 }
 
